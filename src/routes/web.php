2c95b987aab96d2f41fe2c13d869e513cf3e8a8e
Sebastian Robles <grobles@git.ingiga.com>
2021-10-20 12:50:58 -0500

    Add models

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,12 +3,12 @@
 Route::group(['namespace' => 'Capmega\UserCrud\Controllers', 'middleware' => ['web']], function(){
     
     /*Crear CRUD User*/
-	Route::get('/user',                     'UserController@get');
-	Route::get('/user/create',              'UserController@create');
-	Route::post('/user/store',              'UserController@store');
-	Route::get('/user/{id}/details',        'UserController@details');
-	Route::post('/user/{id}/saveDetails',   'UserController@saveDetails');
-	Route::post('/user/{id}/delete',        'UserController@delete');
-	Route::get('/user/export/csv',          'UserController@exportToCSV');
-
+	Route::get('/user',                     'UserController@get')->middleware('level:15');
+	Route::get('/user/create',              'UserController@create')->middleware('level:15');
+	Route::post('/user/store',              'UserController@store')->middleware('level:15');
+	Route::get('/user/{id}/details',        'UserController@details')->middleware('level:15');
+	Route::post('/user/{id}/saveDetails',   'UserController@saveDetails')->middleware('level:15');
+	Route::post('/user/{id}/delete',        'UserController@delete')->middleware('level:15');
+	Route::get('/user/export/csv',          'UserController@exportToCSV')->middleware('level:15');
+	
 });
