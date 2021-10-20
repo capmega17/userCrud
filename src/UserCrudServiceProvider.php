09cec753b0e8f799928d4e62c0646a0a761d716b
Sebastian Robles <grobles@git.ingiga.com>
2021-10-20 11:16:27 -0500

    Fix routes

--- src/UserCrudServiceProvider.php
+++ src/UserCrudServiceProvider.php
@@ -16,11 +16,11 @@ class UserCrudServiceProvider extends ServiceProvider
         $this->publishes([__DIR__.'/../public/js/controllers' => public_path('js/controllers'),], 'users');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'users');
         $this->publishes([__DIR__.'/../resources/views' => resource_path('views/'),], 'users');
-        include __DIR__.’/routes.php’;
+        include __DIR__.'/routes.php';
         $this->publishes([__DIR__.'/Controllers' => app_path('Http/Controllers'),
     ]);
     }
-    
+
     /**
      * Register services.
      *
